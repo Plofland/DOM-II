35f0ac354dc0553d636fdcf0adcea6aa683d6d4d
Plofland <ploflan@gmail.com>
2020-12-01 22:27:10 -0500

    9 out of 10 objectives completed

--- js/index.js
+++ js/index.js
@@ -1,29 +1,88 @@
-// create variables
-// Buttons Why is it not selecting all of the buttons?
-const clickButton = document.getElementsByClassName(".btn");
-console.log(clickButton);
+// Create Variables---------------------------------------
+
+// Home Container
+const home = document.querySelector(".home");
+// Buttons 
+const clickButton = document.querySelectorAll(".btn");
+let clickButtonArray = Array.from(clickButton);
+// Links
+const links = document.querySelectorAll(".nav-link");
+let linksArray = Array.from(links);
+// Fun Bus logo h1
+const funBus = document.querySelector(".logo-heading");
 // Images
-const imgDBClick = document.getElementsByTagName("img");
-console.log(imgDBClick);
+const imgDBClick = document.querySelectorAll("img");
+let imgDBClickArray = Array.from(imgDBClick);
 
 
+//--------------------------------------------------------------
+// On button click, change the color to red
 
-// make the buttons flash on click
-
-clickButton.addEventListener("mouseover", function(event){
-    console.log("clicked");
+clickButtonArray.forEach(elem => {
+    elem.addEventListener('click', () => {
+        elem.style.color = "red";
+    });
 });
 
 
-// on load, make the "Fun Bus" h1 element transition in (make big and then go back to it's normal size)
+// -------------------------------------------------------------------
+// On button double click, send an alert message
+clickButtonArray.forEach((e)=> e.addEventListener('dblclick', ()=> alert("You only need to click the button once.")));
 
-// change the color of the scroll wheel as we scroll
 
-// When an image is double clicked, it's size is changed
-
-imgDBClick.addEventListener("click", function(event){
-    debugger;
-    console.log("double-clicked")
+// -------------------------------------------------------------------
+// Welcome message on a successful page load
+document.addEventListener('keydown', (elem) => {
+    if (elem.key === 'Escape'){
+        funBus.style.backgroundColor = "black"
+        // setInterval(function(funBus.style.backgroundColor.toggle("black")), 1000)
+    }
 });
 
-// When a link is moused over, it will increase in size
+
+
+
+// ----------------------------------------------------------------
+// Change the size of the h1 any time the scroll wheel is used
+document.addEventListener('wheel', () => funBus.style.fontSize = "5rem")
+
+
+// -------------------------------------------------------------
+
+// When an image is double clicked, it applies a gray filter
+
+imgDBClickArray.forEach((item) => 
+    item.addEventListener("click", () => 
+        item.style.opacity = "0.5")
+);
+
+
+// -------------------------------------------------------------
+// When a link is moused over, it will change its color
+
+linksArray.forEach((elem) => 
+    elem.addEventListener('mouseover', () => 
+        elem.style.color = "yellow")
+);
+
+// ----------------------------------------------------------------
+// Change the text color back after mouse hovers off of links
+linksArray.forEach((elem) => 
+    elem.addEventListener('mouseout', () => 
+        elem.style.color = "black")
+);
+
+// ---------------------------------------------------------------
+// Change background color to slate gray when mouse is clicked
+home.addEventListener('mousedown', () =>
+    home.style.backgroundColor = "slateGray"
+);
+
+// ----------------------------------------------------------------
+// Change background color back to white when mouse click is released
+home.addEventListener('mouseup', () =>
+    home.style.backgroundColor = "white"
+);
+
+// ------------------------------------------------------------------
+// 
